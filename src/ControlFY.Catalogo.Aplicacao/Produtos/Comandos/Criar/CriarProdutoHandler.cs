5ec51146ae92912ac171c58375647aa47308793d
Lucas Brendon <65317316+LucasBrendon@users.noreply.github.com>
2022-09-20 23:26:15 -0300

    Ajustes Api de produtos.

--- src/ControlFY.Catalogo.Aplicacao/Produtos/Comandos/Criar/CriarProdutoHandler.cs
+++ src/ControlFY.Catalogo.Aplicacao/Produtos/Comandos/Criar/CriarProdutoHandler.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using ControlFY.Catalogo.Aplicacao.Comum.Contratos.Repositorio;
-using ControlFY.Catalogo.Aplicacao.Comum.Contratos.Servicos;
+
 using ControlFY.Catalogo.Aplicacao.Produtos.ViewModels;
 using ControlFY.Catalogo.Dominio.Entidades;
 using MediatR;
@@ -13,15 +13,14 @@ namespace ControlFY.Catalogo.Aplicacao.Produtos.Comandos.Criar
     {
         private readonly IProdutoRepositorio _produtoRepositorio;
         private readonly IMapper _mapper;
-        private readonly IProdutoServico _produtoServico;
+
 
         public CriarProdutoHandler(IProdutoRepositorio produtoRepositorio,
-                                   IMapper mapper,
-                                   IProdutoServico produtoServico)
+                                   IMapper mapper)
         {
             _produtoRepositorio = produtoRepositorio;
             _mapper = mapper;
-            _produtoServico = produtoServico;
+
         }
 
         public async Task<ProdutoViewModel> Handle(CriarProdutoComando request, CancellationToken cancellationToken)
@@ -29,8 +28,6 @@ namespace ControlFY.Catalogo.Aplicacao.Produtos.Comandos.Criar
             var produto = new Produto(request.Nome, request.Descricao, request.Valor.Value, request.CategoriaId.Value, request.FornecedorId.Value);
 
             await _produtoRepositorio.Cadastrar(produto);
-
-            _produtoServico.PublicarProdutoNaFila(produto);
 
             return _mapper.Map<ProdutoViewModel>(produto);
         }
